e36d5f9bafe6bca4becb250c9248d54b0dd955c6
test <543358961@qq.com>
2021-07-27 19:10:25 +0800

    style(ui): 封装Element-Plus UI库
    对Element-Plus UI库进行按需引入方式集成，安装 babel-plugin-import 插件 + 配置。

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'element-plus',
+        customStyleName: (name) => {
+          return `element-plus/lib/theme-chalk/${name}.css`;
+        }
+        // customStyleName: (name) => {
+        //   name = name.slice(3)
+        //   return `element-plus/packages/theme-chalk/src/${name}.css`;
+        // }
+      }
+    ]
+  ],
   presets: ['@vue/cli-plugin-babel/preset']
 };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from 'vue';
+import { globalRegister } from './global';
+
 import App from './App.vue';
 
 import router from './router';
 import store from './store';
 
 const app = createApp(App);
+
+// globalRegister(app);
+
+app.use(globalRegister);
 app.use(router);
 app.use(store);
 app.mount('#app');
