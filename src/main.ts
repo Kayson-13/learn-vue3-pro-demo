9e4a126cc7318a9d32ac5225556a0587975fd1ea
wndrs <543358961@qq.com>
2021-08-10 08:52:24 +0800

    fix(whole app): 刷新页面的时候，页面总会显示 not-found 页面
    刷新页面，会从头加载，在main.ts文件中，从上到下加载。由于之前先注册的路由，注册路由的时候，会执行install函数，获取当前路径和 router.routes匹配，此时还未执行
    setupStore() 路由还没有被动态的加载进去，因此永远匹配到的都是not-found。解决方法：先执行 setupStore()，将路由动态加载到
    router.routes中，然后再进行路由注册。此时便能够匹配到正确的路径。

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,14 @@ const app = createApp(App);
 
 app.use(globalRegister);
 app.use(router);
-app.use(store);
+// 将setupStore() 和 app.use(store) 互换位置，为了解决刷新会跑到not-found页面bug
+// 刷新页面 -> 从上到下依次加载本页。
+// 当 app.use(store) 在前面的时候，注册路由会去调用 install 方法，获取到当前的path
+// 去 router.routes 中匹配路径。
+// 但是目前并没有执行 setupStore() 没有动态匹配的路径，因此无法匹配到对应的路径，就匹配到了not-found
+// 虽然后面已经将routes动态加载进去了，但是已经匹配到了 not-found
 setupStore();
+app.use(store);
 
 app.mount('#app');
 
